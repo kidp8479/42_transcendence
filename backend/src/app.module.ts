5c9e847a733ffc0a2593c7014d5b32ba99e80ca1
diade-so <diade-so@f6r8s7.paris.42.school>
2026-06-29 17:45:03 +0200

    chore(backend): begin Project entity design (WIP)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,13 @@ import { Module } from "@nestjs/common";
 
 // import each feature module so NestJS knows it exists
 import { PrismaModule } from "./prisma/prisma.module";
+import { ProjectsModule } from "./projects/projects.module";
 
 @Module({
-  imports: [PrismaModule], // registers all modules, NestJS starts them in order at boot
+  imports: [
+    PrismaModule, // registers all modules, NestJS starts them in order at boot
+    ProjectsModule,
+  ],
   controllers: [], // top-level controllers (most are declared inside their own module)
   providers: [], // top-level services (same as above)
 })
