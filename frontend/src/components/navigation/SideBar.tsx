9562ab9ae647663607497f332c9bfc6156bd9bd2
Cricriiii <cest_bon_les_pates@hotmail.com>
2026-07-12 18:31:18 +0200

    feat(TR-38): implemented sidebar collapsing

--- frontend/src/components/navigation/SideBar.tsx
+++ frontend/src/components/navigation/SideBar.tsx
@@ -1,46 +1,58 @@
 "use client";
 
+import { useState } from "react";
 import {
   Sidebar,
   SidebarItem,
   SidebarItemGroup,
   SidebarItems,
 } from "flowbite-react";
-import { BiBuoy } from "react-icons/bi";
 import { Link } from "@tanstack/react-router";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { MdOutlineDashboard } from "react-icons/md";
-// import { GoFileDirectory } from "react-icons/go";
-// import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { GoFileDirectory } from "react-icons/go";
 
-// <SidebarItem as={Link} to="/location" ...> is used to load only the "workinng space" area, rather than the whole page
+// <SidebarItem as={Link} to="/location" ...> is used to load only the "working space" area, rather than the whole page
 
 // If VSCode is unhappy with `to` (red squiggly line), it seems to be because of Flowbite's implementation of Typescript types.
 // Other than that, the Javascript is properly compiled at runtime.
 
 export function SideBar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
-    <Sidebar>
-      <SidebarItems>
-        <SidebarItemGroup>
-          <SidebarItem as={Link} to="/dashboard" icon={MdOutlineDashboard}>
-            Dashboard
-          </SidebarItem>
-          <SidebarItem as={Link} to="/projects" icon={MdOutlineDashboard}>
-            Projects
-          </SidebarItem>
-        </SidebarItemGroup>
-        {/* <SidebarItemGroup>
-          <SidebarItem href="#" icon={HiChartPie}>
-            Upgrade to Pro
-          </SidebarItem>
-          <SidebarItem href="#" icon={HiViewBoards}>
-            Documentation
-          </SidebarItem>
-          <SidebarItem href="#" icon={BiBuoy}>
-            Help
-          </SidebarItem>
-        </SidebarItemGroup> */}
-      </SidebarItems>
-    </Sidebar>
+    <div
+      className={`relative shrink-0 transition-all duration-300 ${
+        isCollapsed ? "w-0" : "w-64"
+      }`}
+    >
+      <div className="h-full overflow-hidden">
+        <Sidebar aria-label="Sidebar" className="h-full w-64">
+          <SidebarItems>
+            <SidebarItemGroup>
+              <SidebarItem as={Link} to="/dashboard" icon={MdOutlineDashboard}>
+                Dashboard
+              </SidebarItem>
+              <SidebarItem as={Link} to="/projects" icon={GoFileDirectory}>
+                Projects
+              </SidebarItem>
+            </SidebarItemGroup>
+          </SidebarItems>
+        </Sidebar>
+      </div>
+
+      <button
+        type="button"
+        onClick={() => setIsCollapsed((prev) => !prev)}
+        aria-label={isCollapsed ? "Expand sidebar" : "Collapse sidebar"}
+        className="absolute top-1/2 right-0 flex h-10 w-4 translate-x-full -translate-y-full items-center justify-center rounded-r-md border border-l-0 border-gray-700 bg-gray-800 text-gray-400 hover:bg-gray-700 hover:text-gray-200"
+      >
+        {isCollapsed ? (
+          <HiChevronRight size={14} />
+        ) : (
+          <HiChevronLeft size={14} />
+        )}
+      </button>
+    </div>
   );
 }
